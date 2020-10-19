09c85a326435581d27748c5ab7ea98fa0b001136
simketejong <simketejong@gmail.com>
2020-10-19 16:19:36 +0200

    Check hhtp feed

--- ParentNode.js
+++ ParentNode.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var http = require('http');
+var url = require('url');
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -10,3 +12,10 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
